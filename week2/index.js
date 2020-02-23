2f1a2cd236623c0c540049ce94be1289f2be689a
maso369 <60929713+maso369@users.noreply.github.com>
2020-02-23 02:28:31 -0500

    Add files via upload

---
+++ week2/index.js
@@ -0,0 +1,17 @@
+var person = {
+    name: "Bobby",
+    age: 12
+}
+if(person.name.startsWith("B")&&(person.age >= 18)){
+    console.log( person.name + " Can go to the movies." );
+} else {
+    console.log(person.name + " cannot go to the movies.")
+}
+var x = 19
+var y = 30
+if (x > y){
+    console.log ("greater than")
+}
+else{
+    console.log("is not greater than")
+}
