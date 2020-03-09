d186fee72928cfe970008b6dcefa9646bbc7e9b0
maso369 <60929713+maso369@users.noreply.github.com>
2020-03-09 02:10:30 -0400

    Add files via upload

--- week4/javascript_calculator/calculator.js
+++ week4/javascript_calculator/calculator.js
@@ -5,6 +5,21 @@ function addTwoNumbers(num1, num2)
    return " adding " + num1 + " with " + num2 + " gives you a result of "
    + (num1 + num2);
 }
+function subTwoNumbers(num1, num2)
+{
+    return " subtracting " + num1 + " with " + num2 + " gives you a result of "
+    + (num1 - num2)
+}
+function mulTwoNumbers(num1, num2)
+{
+    return " multiplying " + num1 + " with " + num2 + " gives you a result of "
+    + (num1 * num2); 
+}
+function divTwoNumbers(num1, num2)
+{
+    return " dividing " + num1 + " with " + num2 + " gives you a result of "
+   + (num1 / num2);
+}
 var FirstNumber = readLineSync.questionInt("A Number")
 var secondNumber = readLineSync.questionInt("another number");
 var mathOperator = readLineSync.question("pick an operator: add, sub, mul, div,");
@@ -14,11 +29,11 @@ if(mathOperator == "add")
     console.log(addTwoNumbers(FirstNumber,secondNumber));
 } else if(mathOperator == "sub")
 {
-    console.log(addTwoNumbers(FirstNumber,secondNumber))
+    console.log(subTwoNumbers(FirstNumber,secondNumber));
 } else if(mathOperator == "mul")
 {
-    console.log(addTwoNumbers(FirstNumber,secondNumber))
+    console.log(mulTwoNumbers(FirstNumber,secondNumber));
 } else if(mathOperator == "div")
 {
-    console.log(addTwoNumbers(FirstNumber,secondNumber))
+    console.log(divTwoNumbers(FirstNumber,secondNumber));
 }
